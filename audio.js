5cf0a3d6044b53cfb4ac14d47b62a5b43409ff30
theRegex <keinan.jpace@gmail.com>
2016-04-22 15:30:23 -0500

    added player

--- audio.js
+++ audio.js
@@ -1,9 +1,73 @@
-var media = (function($){
-var mediaBox = document.getElementById("mediaHd");
-var canvas = document.createElement('canvas');
-var ctx = canvas.getContext("2d");
+  document.addEventListener("DOMContentLoaded", function(event) {
+   var tracks = [
+   {
+   	name: "Pop Mix",
+   	src: "tracks/333565.mp3"
+   },
+    {
+   	name: "Rock Mix",
+   	src: "tracks/333565.mp3"
+   },
+    {
+   	name: "Rap Mix",
+   	src: "tracks/333565.mp3"
+   },
+]
+
+
+  	var playerCanvas = {
+
+  			container: "#waveform",
+  			barWidth: 4,
+  			fillParent: true,
+  			pixelRatio: 1,
+  			waveColor: "tomato"
+
+  	}
+
+
+
+  	var wavesurfer = WaveSurfer;
 
 
 
 
-})(jQuery)
+
+//  	var wavesurfer = WaveSurfer.create({}) <-- .create() takes in an object
+
+  	//wavesurfer.load('tracks/333565.mp3');
+
+
+
+function pushPlaylist(tracks){
+
+
+
+
+}
+
+
+
+
+  	wavesurfer.on('ready', function () {
+
+    $(".ply").click(function(){
+
+    	wavesurfer.play();
+    })
+
+    $(".stp").click(function(){
+
+    	wavesurfer.stop();
+    })
+
+    $(".ps").click(function(){
+    	wavesurfer.pause();
+    })
+});
+
+  });
+
+
+
+  //wavesurfer.pause(), wavesurfer.skipForward(), wavesurfer.toggleMute()
